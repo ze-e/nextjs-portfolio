8a2ca9c666dc507c502c8fdf1290082f61feb2ac
ze-e <white.noise616@gmail.com>
2023-06-04 02:35:23 -0600

    portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,21 @@ import devFinderImg from "@/assets/devfinder.png";
 
 const projects = [
   {
-  title: "devFinder",
-  blurb: "Find top-notch developers in your vicinity with DevFinder",
-  description: "Find top-notch developers in your vicinity with DevFinder",
-  skills: ["React", "TypeScript"],
-  link: "https://ze-e.github.io/dev_finder/",
-  repo: "https://github.com/ze-e/dev_finder",
-  img: devFinderImg
+    title: "devFinder",
+    blurb: "Find top-notch developers in your vicinity with DevFinder",
+    description: "Find top-notch developers in your vicinity with DevFinder",
+    skills: ["React", "TypeScript"],
+    link: "https://ze-e.github.io/dev_finder/",
+    repo: "https://github.com/ze-e/dev_finder",
+    img: devFinderImg,
   },
   {
-  title: "PledgeLlama",
-  blurb: "",
-  description: "",
-  skills: ["React", "PostGreSQL", "NodeJS", "REST API", "Emotion"],
-  link: "",
-  repo: ""
+    title: "PledgeLlama",
+    blurb: "",
+    description: "",
+    skills: ["React", "PostGreSQL", "NodeJS", "REST API", "Emotion"],
+    link: "",
+    repo: "",
   },
 ];
 
