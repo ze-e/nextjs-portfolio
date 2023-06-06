4d842fea9bd0783dd67fce03b30d7f36e130d89c
ze-e <white.noise616@gmail.com>
2023-06-06 17:42:44 -0600

    fixed some responsive behavior

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -6,7 +6,7 @@ import { v4 as createId } from 'uuid';
 export default function Grid({items, type}) {
   return (
     <div className={styles.container}>
-      {formatedItems.map((item, index) => <div className={styles.itemContainer} key={index}><Card data={item} type={type} /></div>)}
+      {items.map((item, index) => <div className={styles.itemContainer} key={index}><Card data={item} type={type} /></div>)}
     </div>
   )
 }
