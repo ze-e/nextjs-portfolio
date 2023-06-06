551bc31ee9a8d3b54a61cba60c82a7a9c4ef2a84
ze-e <white.noise616@gmail.com>
2023-06-05 23:23:39 -0600

    responsive behavior

--- src/components/LogoImg.jsx
+++ src/components/LogoImg.jsx
@@ -1,15 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from 'react'
 import styles from '@/styles/logoImg.module.scss'
 
-import gearImg from "@/assets/gear.gif";
-import textImg from "@/assets/text-2.gif";
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default function LogoImg() {
   return (
     <div className={styles.imgContainer}>
-      <Image src={textImg} alt="logo" className={styles.imgForeground} />
-      <Image src={gearImg} alt="logo" className={styles.imgBackground} />
+      <img src="images/text.gif" alt="logo-name" className={styles.imgForeground} />
+      <img src="images/gear.gif" alt="logo-gear" className={styles.imgBackground} />
     </div>
   )
 }
