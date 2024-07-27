a5d89e1d79db4c394d9fe40485c1292f68086b77
ZEE <white.noise616@gmail.com>
2024-07-26 20:15:19 -0600

    fix import paths

--- src/components/HideOnMobile.jsx
+++ src/components/HideOnMobile.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "@/styles/HideOnMobile.module.scss"
+import styles from "@/styles/hideonmobile.module.scss"
 export default function HideOnMobile({children}) {
   return (
     <div className={styles.container}>
