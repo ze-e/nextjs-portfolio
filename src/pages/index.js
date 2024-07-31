21e44556094411f71136943e509aab9a1e1b0294
ZEE <white.noise616@gmail.com>
2024-07-30 20:49:15 -0600

    fixed scrolling

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function index() {
   return (
     <Layout>
       <Margin>
-        <section className="anchor" id="Home" style={{ marginTop: "100px" }}>
+        <section className="anchor" id="Home" style={{ paddingTop: "120px" }}>
           <TwoColumn
             column_1_flex={2}
             column_2_flex={1}
@@ -103,6 +103,7 @@ export default function index() {
         </video>
       </div>
       <section className="anchor" id="Services">
+        <Divider height={"20px"} background={"#00262D"} />
         <TextBox
           style={{
             display: "flex",
