0ee4263e4441edd0b0b5858cda4d1c74a30aa4c5
ZEE <white.noise616@gmail.com>
2024-10-11 23:21:14 -0600

    testimonial sty;e

--- src/components/TestimonialSlide.jsx
+++ src/components/TestimonialSlide.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from "@/styles/testimonialslide.module.scss"
+import React from 'react';
+import styles from "@/styles/testimonialslide.module.scss";
 import { eduVicWaNtBeginner } from "@/utils/fonts";
 import Image from 'next/image';
 
@@ -7,7 +7,7 @@ export default function TestimonialSlide({data}) {
   return data && (
     <div className={`${styles.container} ${eduVicWaNtBeginner.variable}`}>
       <div className={styles.quoteContainer}>
-        <div className={styles.quote}>
+      <div className={styles.quote}>
           {data?.testimonial}
           <div className={styles.attribution}>
             {" "}--{" "}{data?.name}<br/>
@@ -16,10 +16,10 @@ export default function TestimonialSlide({data}) {
           </div>
         </div>
         <div className={styles.image}>
-          {data?.img && (<Image src={data.img} alt={data.name}/>)}
+          {data?.img && (<Image src={data.img} alt={data.name} />)}
         </div>
-      </div>
 
+      </div>
     </div>
-    )
+  );
 }
